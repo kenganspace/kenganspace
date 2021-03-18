8aa1448c04ec153df229bfe203c152fd07df3a40
KENGAN <weisez@ukr.net>
2021-03-18 22:11:18 +0200

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//alert("Hello World!");
+
+/*let answer = confirm("Are you here?");
+console.log(answer);        // Способ для получения булиновых значений */
+
+//let answer = prompt("Есть ли вам 18", "Да");
+
+console.log(typeof(null)); //Получаем объект с null благодаря typeof, баг JS
